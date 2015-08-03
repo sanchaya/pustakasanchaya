e3d15e0a098499e924bce46ed6b5b064c92297fa
devudilip <devu.dilip@gmail.com>
2015-08-03 21:52:29 +0530

    jquery for home page number incremental dispaly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,25 @@ $( document ).ready( function () {
 	// Kannada ime enabled for form 
 	$( '.kan-ime' ).ime();
 
+// Jquery for home page number count incremental display
+(function($) {
+	"use strict";
+	function count($this){
+		var current = parseInt($this.html(), 10);
+		current = current + 1; /* Where 50 is increment */  
+		$this.html(++current);
+		if(current > $this.data('count')){
+			$this.html($this.data('count'));
+		} else {    
+			setTimeout(function(){count($this)}, 50);
+		}
+	}         
+	$(".stat-count").each(function() {
+		$(this).data('count', parseInt($(this).html(), 10));
+		$(this).html('0');
+		count($(this));
+	});
+})(jQuery);
+
 });
+
