17c12432f345a24c06880154df977b4e6d3e1817
yogiks <yogesh@kalike.org>
2015-08-31 02:25:36 +0530

    Pagination for search results

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,10 @@ class BooksController < ApplicationController
 
   def index
     begin
-     @books = params && params[:search] && !params[:search].blank? ? JSON.parse(Book.search(params[:search])) : {}
-   rescue
-    @books = {}
-  end
+		@books = params && params[:search] && !params[:search].blank? ? JSON.parse(Book.search(params[:search])) : {}
+		@books = Kaminari.paginate_array(@books).page(params[:page]).per(8) 
+    rescue
+		@books = {}
+    end
 end
 end
