4d1e08a5bb1389e2a9abbb7685c48305f1ae1410
devudilip <devu.dilip@gmail.com>
2016-08-23 23:42:02 +0530

    URL added

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,14 @@
 module BooksHelper
+
+  def archive_url(metadata)
+    # if keyword matches for split then result will always be more than 1
+    metadata.split('archive_url:').count > 1
+  end
+
+  def wikimedia_url(metadata)
+    # if keyword matches for split then result will always be more than 1
+    metadata.split('wikimedia_url:').count > 1
+  end
+
+
 end
